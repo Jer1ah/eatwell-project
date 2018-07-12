11e79e31859c75035f07579d5f044b6bdbead902
Jeriah Bowers <jeriah97bowers@yahoo.com>
2018-07-11 21:58:53 -0400

    making site mobile responsive

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const button = document.getElementsByClassName("phone__nav")[0];
+const nav = document.getElementsByClassName("navigation")[0];
+
+button.addEventListener("click", () => {
+    if( nav.style.display != "flex" ) {
+        nav.style.display = "flex";
+    } else if ( nav.style.display == "flex" ) {
+        nav.style.display = "none";
+    }
+});
